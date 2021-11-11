bc467372314415d1eb45cb1f0b377b65cb63c597
master master <34221874+almadrasa@users.noreply.github.com>
2021-11-11 15:03:23 +0000

    final edition V1 fixing isfinal fixing solve showing result fixing fix B coluomn

--- table.js
+++ table.js
@@ -4,11 +4,14 @@ class Table {
     ){
     this.nVar=nVar;
     this.nCon=nCon;
+    this.S=new Array(nCon).fill(null);
+    this.z=0;
     this.C=[0,...C,...new Array(nCon).fill(0)];
     this.b=b;
     let ideA=ide(nCon);
     this.pvI=null;
     this.pvJ=null;
+    this.pivo=null;
     this.A=A.slice(0);
     this.A.forEach((a,i)=>a.push(...ideA[i]));
 
@@ -16,6 +19,9 @@ class Table {
     this.arr.forEach((a,i)=>a.unshift(b[i]));
     
     this.arr.push(this.C);
+    this.tables=[this.ar2O(this.arr)];
+    this.o=this.tables[0];
+     this.solve();
   }
 
   calcPivo(){
@@ -26,21 +32,64 @@ class Table {
     );
     b.pop();
     this.pvI=b.indexOf(Math.min(...b));
+    this.pivo=this.arr[this.pvI][this.pvJ];
+    this.pvL=this.arr[this.pvI];
+    let s=this.arr.map((e,i)=>e[i][0]);
+    s.pop();
+    /*if(this.pvJ<=this.nVar)*/this.S[this.pvI]=this.pvJ;
   }
 
-  show(){
-    let arr=this.arr;
-    arr=arr.reduce((ac,e,i)=>
+  solve(){
+    this.show();
+    this.calcPivo();
+    this.arr.forEach((e,i,arr)=>{
+      e.forEach((x,j)=>{
+        if(i==this.pvI||j==this.pvJ)return;
+        e[j]=x-n((e[this.pvJ]*this.pvL[j])/this.pivo);
+      })
+    });    
+    this.pvL.forEach((e,i,ar)=>ar[i]=n(e/this.pivo));
+    this.arr.forEach((e,i)=>e[this.pvJ]=i!=this.pvI?0:e[this.pvJ]);
+    this.save();
+    if(this.isFinal()){
+      let s=this.arr.map((e,i)=>e[0]);
+      //console.log('s:',s);
+      //console.log('S:',this.S);
+      let lo=this.S.filter(e=>e).map((e,i)=>'x'+e+' = '+s[i]);
+      console.log(lo.join('\n'));
+      this.z=s.pop();
+      console.log('z = ',this.z);
+    }else if(this.tables.length<6) this.solve();
+    else console.log('no optimal solution')
+  }
+
+  isFinal(){
+    let c=this.arr[this.nCon];
+    //console.log(c,c.every((e,i)=>i&&e<=0));
+    return c.every((e,i)=>e<=0);
+  }
+
+  save(){
+    this.o=this.ar2O(this.arr);
+    this.tables.push(this.o);
+  }
+
+  ar2O(arr){
+    return arr.reduce((ac,e,i)=>
       {
         let indx=i!=arr.length-1?'x'+(i+3):'z';
-        if(this.pvJ&&i==this.pvI)indx='x'+this.pvJ;
+        //if(this.pvJ&&i==this.pvI)indx='x'+this.pvJ;
+        if(this.S[i])indx='x'+this.S[i];
         ac[indx]=new line(e);return ac}
     ,{});
-
-    console.table(
-    arr
-    )    
   }
+
+  show(obj=this.o){
+    console.table(obj);
+  }
+  showAll(){
+    this.tables.forEach((e,i)=>console.log(i+1),console.table(obj));
+  }  
 }
 
 function line(carr){
@@ -59,5 +108,7 @@ function ide(nCon=3){
   return arr
 }
 
-
+function n(n){
+  return Number(n.toFixed(2));
+}
 module.exports=Table;
